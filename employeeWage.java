d247096b143d57b815dd31864d4486b69d4c7166
aishwarya-stack <aishwaryaashokkadam@gmail.com>
2021-09-18 12:15:16 +0530

    ADD:employee present or absent

--- employeeWage.java
+++ employeeWage.java
@@ -5,6 +5,18 @@ public class employeeWage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
   System.out.print("Welcome to Employee Wage Computation Program on Master Branch");
+  int number=(int) (Math.floor(Math.random()*10)%2);
+  System.out.println("value"+number);
+  if(number==1)
+  {
+	   System.out.println("employee present");
+	   
+  }
+  else
+  {
+	   System.out.println("employee absent");
+  }
+  
 	}
 
 }
