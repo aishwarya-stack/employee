d8fb5e9e15ce6112518a5745f6c3cb164355ac9c
aishwarya-stack <aishwaryaashokkadam@gmail.com>
2021-09-18 12:43:48 +0530

    ADD:Employee wage using switch case

--- employeeWage.java
+++ employeeWage.java
@@ -1,43 +1,27 @@
 package employeeWage;
 import java.util.*;
 public class employeeWage {
-
+public static final int IS_PART_TIME=1;
+public static final int IS_FULL_TIME=2;
+public static final int EMP_RATE_PER_HOUR=20;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int IS_FULL_TIME=2;
-int IS_PART_TIME=1;
-int EMP_RATE_PER_HOUR=20;
+
 int empHrs=0;
 int empWage=0;
   System.out.print("Welcome to Employee Wage Computation Program on Master Branch");
-  int number=(int) (Math.floor(Math.random()*10)%2);
-  System.out.println("value"+number);
-  if(number==1)
-  {
-	   System.out.println("employee present");
-	   
-  }
-  else
-  {
-	   System.out.println("employee absent");
-  }
   int empCheck=(int) (Math.floor(Math.random()*10)%3);
-  //System.out.println("value"+number);
-  if(empCheck==IS_PART_TIME)
+ 
+  switch (empCheck)
   {
-      empHrs=4;	  
-	   
-  }
-  else if(empCheck==IS_FULL_TIME)
-  {
+  case IS_PART_TIME:
+	  empHrs=4;
+  case IS_FULL_TIME:
 	  empHrs=8;
+  default:
+	  empHrs=4;
   }
-  else 
-  {
-       empHrs=0;	  
-	   System.out.println("employee absent");
-  }
-  empWage=empHrs*EMP_RATE_PER_HOUR;
+   empWage=empHrs*EMP_RATE_PER_HOUR;
  System.out.println("emp wage"+empWage);
 	}
 
