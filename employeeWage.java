98cf25571eb6b8a00f378d1af0ca24319d8ce623
aishwarya-stack <aishwaryaashokkadam@gmail.com>
2021-09-18 12:09:03 +0530

    ADD:new welcome

---
+++ employeeWage.java
@@ -0,0 +1,10 @@
+package employeeWage;
+import java.util.*;
+public class employeeWage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  System.out.print("Welcome to Employee Wage Computation Program on Master Branch");
+	}
+
+}
