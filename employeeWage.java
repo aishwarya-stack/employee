2a6e0463e730530af03d5832b0e53539f6f19acd
aishwarya-stack <aishwaryaashokkadam@gmail.com>
2021-09-18 12:33:51 +0530

    ADD:Add part time employee

--- employeeWage.java
+++ employeeWage.java
@@ -4,7 +4,8 @@ public class employeeWage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int IS_FULL_TIME=1;
+int IS_FULL_TIME=2;
+int IS_PART_TIME=1;
 int EMP_RATE_PER_HOUR=20;
 int empHrs=0;
 int empWage=0;
@@ -20,15 +21,18 @@ int empWage=0;
   {
 	   System.out.println("employee absent");
   }
-  int empCheck=(int) (Math.floor(Math.random()*10)%2);
+  int empCheck=(int) (Math.floor(Math.random()*10)%3);
   //System.out.println("value"+number);
-  if(empCheck==1)
+  if(empCheck==IS_PART_TIME)
   {
-      empHrs=8;	  
-	   System.out.println("employee present");
+      empHrs=4;	  
 	   
   }
-  else
+  else if(empCheck==IS_FULL_TIME)
+  {
+	  empHrs=8;
+  }
+  else 
   {
        empHrs=0;	  
 	   System.out.println("employee absent");
