07aa3e956d49379c8f03c148f8954e6f48cc7de8
aishwarya-stack <aishwaryaashokkadam@gmail.com>
2021-09-18 12:28:11 +0530

    ADD:Calculate daily employee wage

--- employeeWage.java
+++ employeeWage.java
@@ -4,6 +4,10 @@ public class employeeWage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+int IS_FULL_TIME=1;
+int EMP_RATE_PER_HOUR=20;
+int empHrs=0;
+int empWage=0;
   System.out.print("Welcome to Employee Wage Computation Program on Master Branch");
   int number=(int) (Math.floor(Math.random()*10)%2);
   System.out.println("value"+number);
@@ -16,7 +20,21 @@ public class employeeWage {
   {
 	   System.out.println("employee absent");
   }
-  
+  int empCheck=(int) (Math.floor(Math.random()*10)%2);
+  //System.out.println("value"+number);
+  if(empCheck==1)
+  {
+      empHrs=8;	  
+	   System.out.println("employee present");
+	   
+  }
+  else
+  {
+       empHrs=0;	  
+	   System.out.println("employee absent");
+  }
+  empWage=empHrs*EMP_RATE_PER_HOUR;
+ System.out.println("emp wage"+empWage);
 	}
 
 }
